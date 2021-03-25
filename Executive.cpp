bf4a3cf7b6e5e78646a87a456023cc94a9c1dd17
Connor Sullivan <connor.sullivan13@ku.edu>
2021-03-25 17:09:55 -0500

    Add Executive constructor and destructor

--- Executive.cpp
+++ Executive.cpp
@@ -1,1 +1,19 @@
 #include "Executive.h"
+#include <vector>
+#include <fstream>
+
+
+
+Executive::Executive()
+{
+    m_restVector = new std::vector<Restaurant*>;
+}
+
+
+Executive::~Executive()
+{
+    for(int i = 0; i < m_restVector->size(); i++) {
+        delete m_restVector->at(i);
+    }
+    delete m_restVector;
+}
--- Executive.h
+++ Executive.h
@@ -1,4 +1,20 @@
 #ifndef EXECUTIVE_H
 #define EXECUTIVE_H
+#include "Restaurant.h"
+#include <vector>
+
+class Executive
+{
+
+    private:
+    std::vector<Restaurant*>* m_restVector;
+
+    public:
+    Executive();
+    ~Executive();
+    void run();
+
+};
+
 
 #endif
