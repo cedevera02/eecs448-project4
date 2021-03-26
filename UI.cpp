d0b4ced6ad2013b1b423f6cadb2213cba505d8c5
josiezxh <josiejharding@gmail.com>
2021-03-26 13:55:27 -0500

    setup for UI class

--- UI.cpp
+++ UI.cpp
@@ -1,1 +1,25 @@
 #include "UI.h"
+
+UI::UI(Restaurant** ptr)
+{}
+
+void UI::printMenu()
+{}
+
+void UI::printAll()
+{}
+
+void UI::printByCuisine(int choice)
+{}
+
+void UI::printByRating()
+{}
+
+void UI::printRandom()
+{}
+
+void UI::giveRating()
+{}
+
+void UI::removeRestaurant()
+{}
--- UI.h
+++ UI.h
@@ -1,4 +1,23 @@
 #ifndef UI_H
 #define UI_H
+#include "Restaurant.h"
+#include <iostream>
+
+class UI
+{
+    public:
+    UI(Restaurant** ptr); // UI object constructor, store _ResVecPtr member variable
+    void printMenu();
+    void printAll();
+    void printByCuisine(int choice);
+    void printByRating();
+    void printRandom();
+    void giveRating();
+    void removeRestaurant();
+
+    private:
+    Restaurant** _ResVecPtr;
+
+};
 
 #endif
